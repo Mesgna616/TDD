141ad6b4a2f1205fa1eb1570890d9cad38a6be19
mesgna woldeb <mesgna.mehari8@gmail.com>
2023-05-12 11:34:19 +0100

    test passed

--- rps.test.js
+++ rps.test.js
@@ -1,13 +1,14 @@
-function rps() {}
+function rps() {
+  return "left";
+}
 
 describe("rock, paper, scissors", () => {
-    it("should say left wins for rock vs. scissors", () => {
-      const left = "rock";
-      const right = "scissors";
-  
-      const outcome = rps(left, right);
-  
-      expect(outcome).toBe("left");
-    });
+  it("should say left wins for rock vs. scissors", () => {
+    const left = "rock";
+    const right = "scissors";
+
+    const outcome = rps(left, right);
+
+    expect(outcome).toBe("left");
   });
-  
+});
